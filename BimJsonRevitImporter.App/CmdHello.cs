13cde6349a835501ffa13ce130094a7b0ab89f9e
Yehudit-AG <yehudit@agamim-gross.co.il>
2026-02-12 14:09:57 +0200

    initial commit

---
+++ BimJsonRevitImporter.App/CmdHello.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace BimJsonRevitImporter.App
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CmdHello : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("BimJsonRevitImporter", "Loaded OK");
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ BimJsonRevitImporter.App/RevitApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using Autodesk.Revit.UI;
+
+namespace BimJsonRevitImporter.App
+{
+    public class RevitApp : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication app)
+        {
+            const string tabName = "BimBot";
+            const string panelName = "BIMJSON";
+
+            try { app.CreateRibbonTab(tabName); } catch { /* tab may already exist */ }
+
+            var panel = app.CreateRibbonPanel(tabName, panelName);
+
+            string assemblyPath = Assembly.GetExecutingAssembly().Location;
+            string cmdClass = typeof(CmdHello).FullName;
+
+            var btn = new PushButtonData(
+                "BimJsonHello",
+                "Hello\nImporter",
+                assemblyPath,
+                cmdClass
+            );
+
+            panel.AddItem(btn);
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication app) => Result.Succeeded;
+    }
+}
